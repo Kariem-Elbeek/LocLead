2881549222a9632c3a69061c10bebb3f5b0cef63
Kariem88 <abdelkariem.asaad@gmail.com>
2023-10-24 21:09:05 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let ueberUnsRight = document.querySelector(".ueber-uns .right");
+let ueberUnsLeft = document.querySelector(".ueber-uns .left");
+
+console.log(ueberUnsLeft);
+let list = ueberUnsLeft.classList;
+// ueberUnsRight.addEventListener("scroll", function (){ueberUnsLeft.classList.remove("left");})
+
+// function (){ueberUnsLeft.classList.remove("left");}
