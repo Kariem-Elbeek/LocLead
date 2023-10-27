fd053fa3bbaed09d0adb3c66dcac39cdc97243ee
unknown <abdelkariem.asaad@gmail.com>
2023-10-28 01:27:41 +0200

    third

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
+// navbar
+
+$(window).scroll(function(){
+    let windowHeight = $(window).scrollTop();
+    let homeHeight = $('#home').height();
+    if (windowHeight > homeHeight){
+        $('#navbar').addClass('bg-light');
+        $('#navbar').removeClass('bg-white');
+        console.log('done')
+    }
+    else{
+        $('#navbar').addClass('bg-transparent');
+        $('#navbar').removeClass('bg-white'); 
+    }
+});
 
