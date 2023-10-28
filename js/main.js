740bfad28983d14214635a22cdefe9df599590e8
Kariem88 <abdelkariem.asaad@gmail.com>
2023-10-28 16:52:17 +0200

    fourth

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
+// variables
+let navbarHeight = $('#navbar').height();
+let homeHeight = $('#home').height();
+let ueberUnsTop = $('#ueber-uns').offset().top;
+let dienstleistungenTop = $('#dienstleistungen').offset().top;
+
+
 // navbar
 
 $(window).scroll(function(){
     let windowHeight = $(window).scrollTop();
-    let homeHeight = $('#home').height();
     if (windowHeight > homeHeight){
         $('#navbar').addClass('bg-light');
         $('#navbar').removeClass('bg-transparent');
@@ -12,4 +18,34 @@ $(window).scroll(function(){
         $('#navbar').removeClass('bg-light'); 
     }
 });
+
+
+// section ueber-uns
+
+$('#ueber-uns .content-left').css('transform', 'translateX(-120%)');
+$('#ueber-uns .content-right').css('transform', 'translateX(120%)');
+
+$(window).scroll(function () { 
+    let windowHeight = $(window).scrollTop() + navbarHeight;
+    if (windowHeight > ueberUnsTop * .5) {
+        $('#ueber-uns .content-left').css('transform', 'translateX(0%)');
+        $('#ueber-uns .content-right').css('transform', 'translateX(0%)');
+    }
+});
+
+// section ueber-uns
+
+$('#dienstleistungen .content .mycontainer').css('display', 'none');
+$(window).scroll(function () { 
+    let windowHeight = $(window).scrollTop() + navbarHeight;
+    if (windowHeight > dienstleistungenTop * .5) {
+        $('#dienstleistungen .content .mycontainer').slideDown();
+    }
+});
+
+
+
+
+
+
 
