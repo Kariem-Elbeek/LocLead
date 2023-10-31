b332d2d46d504c028c2e50b656c6131f8aceec61
Kariem88 <abdelkariem.asaad@gmail.com>
2023-10-31 18:50:23 +0200

    scroll edit

--- js/main.js
+++ js/main.js
@@ -54,20 +54,17 @@ $('#dienstleistungen .content .content-part').css({'display': 'none'});
 $(window).scroll(function () { 
     let windowTop = $(window).scrollTop();
     let windowBottom = windowTop + $(window).height();
-        if (windowBottom > dienstTop + (dienstHeadingHeight * 1.5)) {
-            if(windowWidth >= 768) {
-                $('#dienstleistungen .content .content-part').slideDown(2000);
-            }
-            else {
-                $('#dienstleistungen .content .content-part').each(function(index) {
-                    var element = $(this);
-                    setTimeout(function() {
-                        element.slideDown(1000);
-                    }, index * 1000); // Delay each animation based on the index
-                });
-            }
-        }
-        
+    if (windowWidth >= 768 && windowBottom > dienstTop + (dienstHeadingHeight * 1.5)) {
+        $('#dienstleistungen .content .content-part').slideDown(2000);
+    }
+    else if (windowWidth < 768 && windowBottom > dienstTop + dienstHeadingHeight) {
+        $('#dienstleistungen .content .content-part').each(function(index) {
+            var element = $(this);
+            setTimeout(function() {
+                element.slideDown(1000);
+            }, index * 1000); // Delay each animation based on the index
+        });
+    }
 });
 
 // section referenzen
