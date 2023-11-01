4fa5602aed56776fa5228c4ed97beeb04ad7453b
Kariem88 <abdelkariem.asaad@gmail.com>
2023-11-02 01:49:47 +0200

    display fixed

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// variables
+// variables for scroll
 let windowTop;
 let windowBottom;
 let navbarHeight = $('#navbar').height();
@@ -16,23 +16,9 @@ let ueberUnsContentTop = $('#ueber-uns .content').offset().top;
 let dienstContentTop = $('#dienstleistungen .content').offset().top
 let referenzenContentTop = $('#referenzen .content').offset().top
 
-// table temp values
-let currentWindowTop = $('.current-WindowTop');
-let firstUeberTop = $('.first-ueberTop');
-let actualUeberTop = $('.actual-ueberTop');
-let firstDienstTop = $('.first-dienstTop');
-let actualDienstTop = $('.actual-dienstTop');
-let firstRefnzTop = $('.first-refnzTop');
-let actualRefnzTop = $('.actual-refnzTop');
-let firstWindowBottom = $('.first-windowBottom');
-let actualWindowBottom = $('.actual-windowBottom');
-
-let ueberUnsTopCurrent;
-let ueberUnsHeightCurrent = $('#ueber-uns').height();
-let dienstTopCurrent;
-let dienstHeadingHeightCurrent = $('#dienstleistungen .section-heading').height();
-let referenzenTopCurrent;
-
+// variables for navbar
+let navId;
+let goToNavIdTop;
 
 // pre-animation
 
@@ -54,21 +40,6 @@ $(window).scroll(function(){
     windowTop = $(window).scrollTop();
     windowBottom = windowTop + windowHeight;
     navbarShow();
-// table temp
-
-    ueberUnsTopCurrent = $('#ueber-uns').offset().top;
-    dienstTopCurrent = $('#dienstleistungen').offset().top;
-    referenzenTopCurrent = $('#referenzen').offset().top;
-
-    currentWindowTop.html(windowTop.toFixed());
-    firstUeberTop.html(ueberUnsTop.toFixed());
-    actualUeberTop.html(ueberUnsTopCurrent.toFixed());
-    firstDienstTop.html(dienstTop.toFixed());
-    actualDienstTop.html(dienstTopCurrent.toFixed());
-    firstRefnzTop.html(referenzenTop.toFixed());
-    actualRefnzTop.html(referenzenTopCurrent.toFixed());
-    firstWindowBottom.html(windowBottom.toFixed());
-    actualWindowBottom.html();
 
     if (windowWidth >= 768){
         if (windowTop > ueberUnsTop - (ueberUnsHeight * .7)){
@@ -98,8 +69,8 @@ $(window).scroll(function(){
 // navbar
 
 function navbarShow (){
-    let windowHeight = $(window).scrollTop();
-    if (windowHeight > homeHeight - navbarHeight){
+    windowTop = $(window).scrollTop();
+    if (windowTop > homeHeight - navbarHeight){
         $('#navbar').addClass('bg-light');
         $('#navbar').removeClass('bg-transparent');
     }
@@ -110,14 +81,16 @@ function navbarShow (){
 }
 
 $('.nav-link').click(function () { 
-    let navId = $(this).attr('nav-id');
-    let goTop = $(navId).offset().top;
-    window.scrollTo(0, goTop - navbarHeight);
+    navId = $(this).attr('nav-id');
+    goToNavIdTop = $(navId).offset().top;
+    window.scrollTo(0, goToNavIdTop - navbarHeight);
 });
 
 
-// showSmall
 
+// show functions
+
+// showOnSmallWidth
 function showEachSlideDown(element, i) 
     {
         setTimeout(function() 
@@ -147,30 +120,15 @@ function dienstShowBig ()
 {
     console.log("big width = " + windowWidth);
     $('#dienstleistungen .content .content-part').slideDown(2000);
-    $('#dienstleistungen .section-heading h2').addClass('bg-danger');
 }
 
 function dienstShowSmall (){
     console.log("small width = " + windowWidth);
-    $('#dienstleistungen .section-heading h2').addClass('bg-danger');
     $('#dienstleistungen .content .content-part').each(function(i){
         let element = $(this);
         showEachSlideDown(element, i);
     });    
 }
-
-    // if (windowWidth >= 768 && windowBottom > dienstTop + (dienstHeadingHeight * 1.5)) {
-    //     $('#dienstleistungen .content .content-part').slideDown(2000);
-    // }
-    // else if (windowWidth < 768 && windowBottom > dienstTop + dienstHeadingHeight) {
-    //     $('#dienstleistungen .content .content-part').each(function(index) {
-    //         var element = $(this);
-    //         setTimeout(function() {
-    //             element.slideDown(1000);
-    //         }, index * 1000); // Delay each animation based on the index
-    //     });
-    // }
-
 
 // section referenzen
 
