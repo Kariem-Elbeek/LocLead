d4e9222917015d578dd97b3c900208a2894146e0
Kariem88 <abdelkariem.asaad@gmail.com>
2023-11-01 07:29:49 +0200

    scroll edit bg-danger

--- js/main.js
+++ js/main.js
@@ -13,6 +13,12 @@ let refnzHeadingHeight = $('#referenzen .section-heading').height();
 // navbar
 
 $(window).scroll(function(){
+    navbarShow();
+    ueberUnsShow();
+    dienstShow();
+    refnzShow();
+});
+function navbarShow (){
     let windowHeight = $(window).scrollTop();
     if (windowHeight > homeHeight - navbarHeight){
         $('#navbar').addClass('bg-light');
@@ -22,7 +28,7 @@ $(window).scroll(function(){
         $('#navbar').addClass('bg-transparent');
         $('#navbar').removeClass('bg-light'); 
     }
-});
+}
 
 $('.nav-link').click(function () { 
     let navId = $(this).attr('nav-id');
@@ -36,7 +42,8 @@ $('.nav-link').click(function () {
 $('#ueber-uns .content-left').css({'transform': 'translateX(-120%)', 'transition': 'all 1.5s linear'});
 $('#ueber-uns .content-right').css({'transform': 'translateX(120%)', 'transition': 'all 1.5s linear'});
 
-$(window).scroll(function () { 
+// $(window).scroll();
+function ueberUnsShow () { 
     let windowTop = $(window).scrollTop();
     let ueberUnsTop = $('#ueber-uns').offset().top;
     let ueberUnsHeight = $('#ueber-uns').height();
@@ -44,34 +51,41 @@ $(window).scroll(function () {
         $('#ueber-uns .content-left').css('transform', 'translateX(0%)');
         $('#ueber-uns .content-right').css('transform', 'translateX(0%)');
     }
-});
+}
 
 
 
 // section dienstleistungen
 
 $('#dienstleistungen .content .content-part').css({'display': 'none'});
-$(window).scroll(function () { 
+// $(window).scroll();
+function dienstShow () 
+{ 
     let windowTop = $(window).scrollTop();
     let windowBottom = windowTop + $(window).height();
 
-    if (windowWidth >= 768){
-        console.log("big width");
-        if(windowBottom > dienstTop + (dienstHeadingHeight * 1.5)){
+    if(windowBottom > dienstTop + (dienstHeadingHeight * 1.5))
+    {
+        if (windowWidth >= 768)
+        {
+            console.log("big width = " + windowWidth);
             $('#dienstleistungen .content .content-part').slideDown(2000);
             $('#dienstleistungen .section-heading h2').addClass('bg-danger');
         }
-    }
-    else{
-        if(windowBottom > dienstTop){
+        else
+        {
+            console.log("small width = " + windowWidth);
             $('#dienstleistungen .section-heading h2').addClass('bg-danger');
-            $('#dienstleistungen .content .content-part').each(function(index) {
-            var element = $(this);
-            setTimeout(function() {
-                element.slideDown(1000);
-            }, index * 1000); // Delay each animation based on the index
-        });    
-    }
+            $('#dienstleistungen .content .content-part').each(function(index) 
+            {
+                var element = $(this);
+                setTimeout(function() 
+                    {
+                        element.slideDown(1000);
+                    }, index * 1000
+                ); // Delay each animation based on the index
+            });    
+        }
     }
 
     // if (windowWidth >= 768 && windowBottom > dienstTop + (dienstHeadingHeight * 1.5)) {
@@ -85,14 +99,16 @@ $(window).scroll(function () {
     //         }, index * 1000); // Delay each animation based on the index
     //     });
     // }
-});
+}
 
 // section referenzen
 
 $('#referenzen .content-left').css({'transform': 'translateX(-120%)', 'transition': 'all 1.5s linear'});
 $('#referenzen .content-right').css({'transform': 'translateX(120%)', 'transition': 'all 1.5s linear'});
 
-$(window).scroll(function () { 
+// $(window).scroll();
+
+function refnzShow () { 
     let windowTop = $(window).scrollTop();
     let windowBottom = windowTop + windowHeight;
     if (windowBottom > referenzenTop + (refnzHeadingHeight * 1.5)) {
@@ -109,6 +125,6 @@ $(window).scroll(function () {
             });
         }
     }
-});
+}
 
 
